02b94f8aec8ed1247ace9cbb31e519abe71c0907
CN89 <charlieniewiarowski@gmail.com>
2025-02-28 21:30:19 -0600

    server.js file + controller and route folders

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json({mssg: 'welcome to the app'})
+})
+
+app.listen(4000, () => {
+    console.log('currently listening on port 4000')
+})
