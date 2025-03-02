ebbe6958f156fad80d746f3decf244ddb07fee72
CN89 <charlieniewiarowski@gmail.com>
2025-03-01 20:10:03 -0600

    leaving for context

--- backend/server.js
+++ backend/server.js
@@ -27,22 +27,6 @@ try {
 } catch (err) {
     console.log(`Connection error\n${err}\nCause: ${err.cause}`)
 }
-/*
-const connect = (async () => {
-    const URI = 'neo4j+s://7d1a0d62.databases.neo4j.io'
-    const USER = 'neo4j'
-    const PASSWORD = 'SYJog0Ps7HJLMg6cpGSwGxftlfuIjdqreFewtbMaIP8'
-    let driver;
-  
-    try {
-      driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
-      const serverInfo = await driver.getServerInfo()
-      console.log('Connection established')
-      console.log(serverInfo)
-    } catch(err) {
-      console.log(`Connection error\n${err}\nCause: ${err.cause}`)
-    }
-})();*/
 
 module.exports = driver;
 
