bdf4618f78f1192d7523ca15f2e83fa5652dc566
CN89 <charlieniewiarowski@gmail.com>
2025-03-01 21:50:33 -0600

    commit

--- frontend/front-end/app/layout.tsx
+++ frontend/front-end/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { useAuthContext } from './';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const inter = Inter({ subsets: ["latin"] })
 
